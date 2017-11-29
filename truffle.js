21ce5391fefc3916ee27521050398636e73ed8c0
zomlandlin <zomland.lin@imediplus.com>
2017-11-29 18:10:15 +0800

    add update

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
     development: {
 //      host: "127.0.0.1",
       host: "192.168.51.202",
-      port: 8545,
+      port: 8546,
       network_id: "*" // Match any network id
     }
   }
